ce744feedce62dd988ea4da900e11cc8a1d09271
aman <amankrsingh06012001@gmail.com>
2025-08-13 14:27:14 +0530

    array and dict

---
+++ array.py
@@ -0,0 +1,7 @@
+arr = []
+arr.append(123)
+arr.append(234)
+arr.append("aman")  
+print(arr)
+print(f"type of arr[1] is {type(arr[1])}")
+print(f"type of arr[2] is {type(arr[2])}")
---
+++ dic.py
@@ -0,0 +1,18 @@
+# marks={}
+# print(type(marks))
+# marks["maths"]= 94
+# marks["science"]= 63
+# marks["ss"]=62.5
+# # marks[123]=100 /# its works 
+# print(marks)
+# print(f"type of maths marks is{type(marks["maths"])}")
+# print(f"type of ss marks is {type(marks["ss"])}")
+
+
+# array in keyy
+
+list_to_sum = {}
+arr = [1,2,3]
+list_to_sum[arr]= 6
+arr.append(23)
+print(list_to_sum)
